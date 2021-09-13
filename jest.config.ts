23d85b4b1d77bb98e5432caba29ab3c25b395f56
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-09-12 21:55:57 -0300

    add jest

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+export default {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '**/*.spec.ts',
+    '**/*.test.ts'
+  ],
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+  clearMocks: true
+}
---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+describe('Example', () => {
+  it('should sum two numbers', async () => {
+    expect(1 + 1).toBe(2)
+  })
+
+  it('should subtract two numbers', async () => {
+    expect(1 - 1).toBe(0)
+  })
+})
